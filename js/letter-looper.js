babd0ce4f46a66ec90e4b35f908334afc5c847d3
Sarah Zaman <zama0029@algonquinlive.com>
2017-01-30 16:08:14 -0500

    Update letter-looper

--- js/letter-looper.js
+++ js/letter-looper.js
@@ -1,1 +1,16 @@
+var letter;
+var i;
+var k;
+
+while (!letter) {
+    var letter = prompt("Enter a character.");
+    
+    for (i = 0; i < 10; i += 1) {
+        for (k = 0; k < i; k += 1) {
+        document.write(letter);    
+        }
+        
+        document.write("<br>");
+    }
+}
 
